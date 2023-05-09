e0968615271e20109c6b5fdebf980e99afeff3ca
Mano123 <hermann.agossou@yahoo.fr>
2023-05-09 14:53:33 +0200

    Update extraction_syno.py

--- extraction_syno.py
+++ extraction_syno.py
@@ -85,19 +85,19 @@ if uploaded_file is not None:
         MODELE=CHAINE[CHAINE.str.contains('BPEU[0-9]|BPEA[0-9]|BPP[0-9]|BPI[0-9]|BPEI[0-9]|BPE-V|BPEV',na=False)]
 
         for index_modele,value_modele in zip(MODELE.index,MODELE.values):
-            if 'FR6' in CHAINE[index_modele+1]:
+            if 'FR6' in str(CHAINE[index_modele+1]):
                 MODELE_FINAL.append(CHAINE[index_modele+1])
-            elif 'HD' in CHAINE[index_modele+1]:
+            elif 'HD' in str(CHAINE[index_modele+1]):
                 MODELE_FINAL.append(CHAINE[index_modele+1])
-            elif 'T0' in CHAINE[index_modele+1]:
+            elif 'T0' in str(CHAINE[index_modele+1]):
                 MODELE_FINAL.append(CHAINE[index_modele+1])
-            elif 'T1' in CHAINE[index_modele+1]:
+            elif 'T1' in str(CHAINE[index_modele+1]):
                 MODELE_FINAL.append(CHAINE[index_modele+1])
-            elif 'Taille 0' in CHAINE[index_modele+1]:
+            elif 'Taille 0' in str(CHAINE[index_modele+1]):
                 MODELE_FINAL.append(CHAINE[index_modele+1])
-            elif 'PEO' in CHAINE[index_modele+1]:
+            elif 'PEO' in str(CHAINE[index_modele+1]):
                 MODELE_FINAL.append(CHAINE[index_modele+1])
-            elif 'TAILLE 1' in CHAINE[index_modele+1]:
+            elif 'TAILLE 1' in str(CHAINE[index_modele+1]):
                 MODELE_FINAL.append(CHAINE[index_modele+1])
 
         MODELE_FINAL=pd.Series(MODELE_FINAL)
