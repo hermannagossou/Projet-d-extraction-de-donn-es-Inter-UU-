c0791faaa0d71340bcfd9bad234f96b64dd2d955
Mano123 <hermann.agossou@yahoo.fr>
2023-07-04 19:10:56 +0200

    Update ACCUEIL.py

--- ACCUEIL.py
+++ ACCUEIL.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.set_page_config(
+    page_title="ACCUEIL",
+    page_icon="",
+)
+
+st.write("FAITES LE CHOIX DU SERVICE D'EXTRACTION SOUHAITE")
