4d1cb92d3311353dc6e2b8067e9615757b5e3cc6
Mano123 <hermann.agossou@yahoo.fr>
2023-07-06 12:42:51 +0200

    Update EXTRACTION_PDS.py

--- pages/EXTRACTION_PDS.py
+++ pages/EXTRACTION_PDS.py
@@ -28,7 +28,7 @@ if uploaded_file is not None:
             continue
         else:
             #Récupération du champ BPE
-            BPE_CHAINE=PDS[PDS.str.contains('BPEU[0-9]',na=False)]
+            BPE_CHAINE=PDS[PDS.str.contains('BPEU[0-9]',na=False)].drop_duplicates()
             for bpe in BPE_CHAINE:
                 BPE.append(bpe)
         
