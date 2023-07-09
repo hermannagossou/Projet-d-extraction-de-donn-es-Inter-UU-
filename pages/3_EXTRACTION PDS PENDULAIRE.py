dc0a6de6c35c3911beb4d9ae5b2db8cf1f36be9a
Mano123 <hermann.agossou@yahoo.fr>
2023-07-09 15:36:23 +0200

    Update 3_EXTRACTION PDS PENDULAIRE.py

--- pages/3_EXTRACTION PDS PENDULAIRE.py
+++ pages/3_EXTRACTION PDS PENDULAIRE.py
@@ -28,5 +28,6 @@ if uploaded_file is not None:
         BPE.append(PDS[PDS.str.contains('BPEU[0-9]|BPEA[0-9]',na=False)])
         
         st.write(PDS)
+        st.wrtite(PDS[1])
 
 st.write(BPE)
