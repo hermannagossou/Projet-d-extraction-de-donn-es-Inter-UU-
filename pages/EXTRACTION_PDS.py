a3c997aee412aa4a0dd369210bd8bf126eb5c292
Mano123 <hermann.agossou@yahoo.fr>
2023-07-09 15:11:42 +0200

    Update EXTRACTION_PDS.py

--- pages/EXTRACTION_PDS.py
+++ pages/EXTRACTION_PDS.py
@@ -7,7 +7,7 @@ import numpy as np
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
-st.title('EXTRACTION PDS')
+st.title('EXTRACTION PDS DORSALE')
 
 uploaded_file = st.file_uploader("Choisissez un fichier")
 
