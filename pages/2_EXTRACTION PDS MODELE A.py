b381f0106fb0516eb789d6681f4caeb4297b37bf
Mano123 <hermann.agossou@yahoo.fr>
2023-08-08 15:29:02 +0200

    Update 2_EXTRACTION PDS MODELE A.py

--- pages/2_EXTRACTION PDS MODELE A.py
+++ pages/2_EXTRACTION PDS MODELE A.py
@@ -34,8 +34,8 @@ if uploaded_file is not None:
                   continue
                 elif str(element)=='Elément':
                   ELEMENT.append(PDS[index_element+1])
-                elif str(element)=='Lien Inter-UU':
-                  ELEMENT.append(PDS[index_element+1])
+                elif str(element)=='Lien Inter-UU' and str(element)=='ELEMENT LIEN INTER-UU':
+                  print(index_element)
             
             #Récupération du champ BPE
             BPE.append(PDS[PDS.str.contains('BPEU[0-9]|BPEA[0-9]|BPEI[0-9]',na=False)].iloc[0])
