b7dbc418d4e1c9c93744413e48466db045e55506
Mano123 <hermann.agossou@yahoo.fr>
2023-08-29 10:27:14 +0200

    Update 5_Test_FOA.py

--- pages/5_Test_FOA.py
+++ pages/5_Test_FOA.py
@@ -8,5 +8,6 @@ st.write(os.getcwd())
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
+    st.write(bytes_data)
 
 
