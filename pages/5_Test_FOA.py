35720adec2cb1f7f39de0260d36f43cc897ee9c2
Mano123 <hermann.agossou@yahoo.fr>
2023-08-29 10:19:41 +0200

    Update 5_Test_FOA.py

--- pages/5_Test_FOA.py
+++ pages/5_Test_FOA.py
@@ -6,5 +6,5 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-  st.write(stringio)
+  string_data = stringio.read()
+  st.write(string_data)
