7693e78e629153e74a9dfe69ff8874c3def78524
Mano123 <hermann.agossou@yahoo.fr>
2023-08-29 10:22:17 +0200

    Update 5_Test_FOA.py

--- pages/5_Test_FOA.py
+++ pages/5_Test_FOA.py
@@ -2,9 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  string_data = stringio.read()
-  st.write(string_data)
+st.write(os.getcwd())
