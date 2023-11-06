5ed6b9168e6b5b4e4e6f8a69963ec9dfe2df503a
Hermann AGOSSOU <hermann.agossou@yahoo.fr>
2023-11-06 10:11:46 +0100

    Update 1_EXTRACTION SYNOPTIQUE.py

--- pages/1_EXTRACTION SYNOPTIQUE.py
+++ pages/1_EXTRACTION SYNOPTIQUE.py
@@ -150,19 +150,21 @@ if uploaded_file is not None:
                         index_cable.append(index_cab+1)
                     elif 'CDD' in str(CHAINE[index_cab+1]):
                         index_cable.append(index_cab+1)
+                    elif 'CBD' in str(CHAINE[index_cab+1]):
+                        index_cable.append(index_cab+1)
                     elif 'CIU' in str(CHAINE[index_cab+2]):
                         index_cable.append(index_cab+2)
                     elif 'CDP' in str(CHAINE[index_cab+2]):
                         index_cable.append(index_cab+2)
                     elif 'CIC' in str(CHAINE[index_cab+2]):
                         index_cable.append(index_cab+2)
-                    elif 'CBD' in str(CHAINE[index_cab+2]):
-                        index_cable.append(index_cab+2)
                     elif 'CSA' in str(CHAINE[index_cab+2]):
                         index_cable.append(index_cab+2)
                     elif 'CAD' in str(CHAINE[index_cab+2]):
                         index_cable.append(index_cab+2)
                     elif 'CDD' in str(CHAINE[index_cab+2]):
+                        index_cable.append(index_cab+2)
+                    elif 'CBD' in str(CHAINE[index_cab+2]):
                         index_cable.append(index_cab+2)
 
                 for ic in index_cable:
