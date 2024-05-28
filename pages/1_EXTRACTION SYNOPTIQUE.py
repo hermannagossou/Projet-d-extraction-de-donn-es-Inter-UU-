e94f445aace077ef89cf3960ac840d5ffcf3c995
Hermann AGOSSOU <hermann.agossou@yahoo.fr>
2024-05-28 10:45:06 +0200

    Update 1_EXTRACTION SYNOPTIQUE.py

--- pages/1_EXTRACTION SYNOPTIQUE.py
+++ pages/1_EXTRACTION SYNOPTIQUE.py
@@ -92,7 +92,7 @@ if uploaded_file is not None:
                     else:
                         BOITE.append(CHAINE[index_bpe])
 
-                #BPEU=BPEU.reset_index(drop=True)
+                BOITE=pd.Series(BOITE)
 
                 #TRAITEMENT DES MODELES
 
@@ -249,16 +249,16 @@ if uploaded_file is not None:
                 st.write(BOITE)
                 
 
-                #df_out=pd.concat([CABLE,CAPACITE_FINAL,LONGUEUR,BOITE,EPISSURE,MODELE_FINAL,SUPPORTS,ADRESSE_FINAL,REFCOM],axis=1)
-                #df_out=df_out.rename(columns={0:'CABLE',1:'CAPACITE',2:'LONGUEUR',3:'BPEU',4:'EPISSURE',5:'MODELE',6:'SUPPORT',7:'ADRESSE',8:'REF COMMANDE'})
+                df_out=pd.concat([CABLE,CAPACITE_FINAL,LONGUEUR,BOITE,EPISSURE,MODELE_FINAL,SUPPORTS,ADRESSE_FINAL,REFCOM],axis=1)
+                df_out=df_out.rename(columns={0:'CABLE',1:'CAPACITE',2:'LONGUEUR',3:'BPEU',4:'EPISSURE',5:'MODELE',6:'SUPPORT',7:'ADRESSE',8:'REF COMMANDE'})
 
-                #st.subheader(index_df)
-                #st.write(df_out)
-                #csv=convert_df(df_out)
+                st.subheader(index_df)
+                st.write(df_out)
+                csv=convert_df(df_out)
 
-                #st.download_button(
+                st.download_button(
                     #label='Télécharger',
                     #data=csv,
                     #file_name='Syno_'+index_df+'.csv',
                     #mime='text/csv'
-            #)
+            )
